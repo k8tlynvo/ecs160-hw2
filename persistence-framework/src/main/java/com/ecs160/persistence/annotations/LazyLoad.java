454f17ba0527708de4e110f026918197c2b5427c
Mohnish <mohnishgopi@gmail.com>
2025-11-20 00:39:00 -0800

    Part A adding persistence annotations (@PersistableField, @Id, @LazyLoad)

---
+++ persistence-framework/src/main/java/com/ecs160/persistence/annotations/LazyLoad.java
@@ -0,0 +1,10 @@
+package com.ecs160.persistence.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LazyLoad {
+    String field();
+}
+
---
+++ persistence-framework/src/main/java/com/ecs160/persistence/annotations/PersistableField.java
@@ -0,0 +1,9 @@
+package com.ecs160.persistence.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PersistableField {
+}
+
